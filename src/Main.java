af9583194654e26e9c04ba0480d900dee2d2d3e8
Tom Ludwig <tommludwig@icloud.com>
2024-02-08 20:49:20 +0100

    quick sort

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         SortingAlgorithms sortingAlgorithms = new SortingAlgorithms();
-        int[] numbers = {2, 5, 2, 7, 3, 3, 3, 5, 20, 1};
-        int[] sortedNumbers = sortingAlgorithms.bubbleSort(numbers);
-
-        for (int sortedNumber : sortedNumbers) {
+        //int[] numbers = {2, 5, 2, 7, 3, 3, 3, 5, 20, 1};
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        //int[] sortedNumbers = sortingAlgorithms.bubbleSort(numbers);
+        sortingAlgorithms.quickSort(numbers, 0, numbers.length - 1);
+        for (int sortedNumber : numbers) {
             System.out.println(sortedNumber);
         }
+
+
     }
 }
--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -1,5 +1,32 @@
+import java.lang.reflect.Array;
+
 public class SortingAlgorithms {
     public SortingAlgorithms() {}
+
+    public void quickSort(int[] dataSet, int low, int high) {
+        if (low < high) {
+            int pivotIndex = partition(dataSet, low, high);
+            System.out.println("Recursing");
+            quickSort(dataSet, low, pivotIndex - 1);
+            quickSort(dataSet, pivotIndex + 1, high);
+        }
+
+    }
+
+    private int partition(int[] dataSet, int low, int high) {
+        int pivot = dataSet[high];
+        int index = low - 1;
+
+        for (int compareIndex = low; compareIndex <= high; compareIndex++) {
+            if (dataSet[compareIndex] < pivot) {
+                System.out.println(dataSet[compareIndex] + " is less than " + pivot);
+                index++;
+                swapElements(dataSet, index, compareIndex);
+            }
+        }
+        swapElements(dataSet, index + 1, high);
+        return index + 1;
+    }
     public int[] bubbleSort(int[] numbers) {
         for (int i = 0; i < numbers.length - 1; i++) {
             for (int j = 0; j < numbers.length - 1; j++) {
@@ -11,8 +38,10 @@ public class SortingAlgorithms {
                     int temporary = numbers[j];
                     numbers[j] = numbers[j + 1];
                     numbers[j + 1] = temporary;
-                     */
+                    */
 
+                    // XOR swap algorithm
+                    // Array is being passed by reference, so no need to return it;
                     swapElements(numbers, j, j + 1);
                 }
 
@@ -22,8 +51,8 @@ public class SortingAlgorithms {
         return numbers;
     }
 
-
     public static void swapElements(int[] array, int index1, int index2) {
+        System.out.println("Swapping " + index1 + " with " + index2);
         if (index1 == index2) {
             // No need to swap if the indices are the same
             return;
